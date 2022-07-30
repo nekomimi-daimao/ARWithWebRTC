4e45907585dec5f6ae73da6ada31831df242279a
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2022-07-31 01:57:35 +0900

    create ws server

---
+++ wssServer/index.js
@@ -0,0 +1,27 @@
+import {WebSocketServer} from "ws";
+import ip from "ip";
+
+const port = 5555;
+const server = new WebSocketServer({port: port});
+
+server.on('connection', function connection(ws, req) {
+    const remoteAddress = req.socket.remoteAddress;
+    console.log(`connection ${remoteAddress}`);
+
+    ws.on('open', function open() {
+        console.log(`open ${remoteAddress}`);
+    });
+    ws.on('close', function close() {
+        console.log(`close ${remoteAddress}`);
+    });
+
+    ws.on('message', function message(data, isBinary) {
+        server.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data, {binary: isBinary});
+            }
+        });
+    });
+});
+
+console.log(`${ip.address()}:${port}`);
